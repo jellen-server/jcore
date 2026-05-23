4b36f652de1891864071f878d00d563a1c13dad2
Vactor0911 <vactor0911@gmail.com>
2026-05-23 15:39:38 +0000

    에러 핸들러 파라미터 오류 수정

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,8 +1,13 @@
 // middleware/errorHandler.ts
-import { Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import AppError from "../errors/AppError";
 
-export const errorHandler = (err: Error, res: Response): void => {
+export const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): void => {
   let statusCode = 500;
   let message = "Internal server error";
 
@@ -12,10 +17,12 @@ export const errorHandler = (err: Error, res: Response): void => {
     message = err.message;
   }
 
+  // 예외 응답 생성
   const response = {
     success: false,
     error: message,
   };
 
+  // 예외 응답 전송
   res.status(statusCode).json(response);
 };
